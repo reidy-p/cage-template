f475b1181c39acdd830980bf7056c498e4e78a4f
Paul Reidy <paul-reidy@outlook.com>
2021-09-01 20:05:43 +0100

    Adjusted globbing

--- globbing.js
+++ globbing.js
@@ -1,18 +1,23 @@
 const glob = require("glob")
 
-// options is optional
 exports.handler = () => { 
-    glob("/*", function (err, files) {
+    glob("/opt/*", function (err, files) {
     // files is an array of filenames.
     // If the `nonull` option is set, and nothing
     // was found, then files is ["**/*.js"]
     // er is an error object or null.
-    for (filename in files) {
-      console.log(filename);
-    };
+    if (err) {
+        console.log(err)
+    }
+
+    console.log(files);
+    //for (const filename of files) {
+    //  console.log(filename);
+    //};
+
+    })
 
     return {
       success: true,
     }
-  })
 }
