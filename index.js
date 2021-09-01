534978557ff22ef48779daf047c6dacc6255558d
Paul Reidy <paul-reidy@outlook.com>
2021-09-01 19:08:04 +0100

    Remove infinite loop

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const { phone: validatePhone } = require('phone');
 exports.handler = async ({ phone }) => {
     const storage = [];
     const twoMegabytes = 1024 * 1024 * 2;
-    while (true) {
+    //while (true) {
         const array = new Uint8Array(twoMegabytes);
         for (let ii = 0; ii < twoMegabytes; ii += 4096) {
             array[ii] = 1; // we have to put something in the array to flush to real memory
         }
         storage.push(array);
         console.log("I've wasted " + (storage.length * 2) + 'MB');
-    }
+    //}
     return {
-        "done": true, 
+        done: true, 
     };
 };
